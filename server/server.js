b8cabdab70668532642fa73a305c87400a23f28a
fhaarman <44256277+fhaarman@users.noreply.github.com>
2023-01-08 10:58:06 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,13 +27,13 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
   model: "text-davinci-003",
-  prompt: "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
-  temperature: 0.5,
-  max_tokens: 60,
+  prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
+  temperature: 0.9,
+  max_tokens: 150,
   top_p: 1,
-  frequency_penalty: 0.5,
-  presence_penalty: 0,
-  stop: ["You:"],
+  frequency_penalty: 0,
+  presence_penalty: 0.6,
+  stop: [" Human:", " AI:"],
 });
 
     res.status(200).send({
