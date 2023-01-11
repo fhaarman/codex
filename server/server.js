6c2cc9802c9e3d0a4e5478ef686276be59a3d5fb
fhaarman <44256277+fhaarman@users.noreply.github.com>
2023-01-11 14:21:46 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,15 +35,12 @@ model: "text-davinci-003",
       presence_penalty: 0.6, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
-       stop=[" Human:", " AI:"]
-    )
-    story = response['choices'][0]['text']
-    return str(story)
-
 def append_interaction_to_chat_log(question, answer, chat_log=None):
     if chat_log is None:
         chat_log = session_prompt
     return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
+    
+    }),
 
   } catch (error) {
     console.error(error)
