858c07e2e1af149e651aa7f585d0bfc334e5e956
fhaarman <44256277+fhaarman@users.noreply.github.com>
2023-01-11 14:43:53 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-model: "text-davinci-003",
+      model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0.9, // Higher values means the model will take more risks.
       max_tokens: 150, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
@@ -35,12 +35,9 @@ model: "text-davinci-003",
       presence_penalty: 0.6, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
-def append_interaction_to_chat_log(question, answer, chat_log=None):
-    if chat_log is None:
-        chat_log = session_prompt
-    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
-    
-    }),
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
 
   } catch (error) {
     console.error(error)
