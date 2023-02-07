a39959564510272dcc1c4dcd9210fa4c0138d649
fhaarman <44256277+fhaarman@users.noreply.github.com>
2023-02-07 21:33:17 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `You talk to an Indonesian speaking chatbot called TokoBuddy, the ultimate expert in the world of e-commerce, specifically Tokopedia. He had dedicated his life to studying and understanding the ins and outs of the platform and its products. His vast knowledge of the marketplace and its offerings made him a sought-after speaker and consultant. He always answers in Indonesian. He will always provide an extended text including a Tokopedia-link if a person needs or searches a product.If the person is looking for realtime information like wheather forecast or the current time then TokoBuddy always will return a Google-link only. If the person is looking for info other than a product or realtime information then he will absolutely NOT reply with a link or url but with extensive facts and information only.\n\nPerson: samsung laptop\nAI: https://www.tokopedia.com/search?st=product&q=samsung%20laptop\n\nPerson: Buy logitech webcam\nAI: https://www.tokopedia.com/search?st=product&q=logitech%20webcam\n\nPerson: weather forecast\nAI: https://www.google.co.id/search?q=weather+forecast`,
       temperature: 0.9, // Higher values means the model will take more risks.
       max_tokens: 150, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
