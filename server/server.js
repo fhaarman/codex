2ddf5b921db830930e30d6225387ee60d9fc69d8
fhaarman <44256277+fhaarman@users.noreply.github.com>
2023-02-08 17:13:01 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `This is a conversation between ChatGPTBuddy and a stranger.\nRelevant information that ChatGPTBuddy knows:\n${prompt}`,
       temperature: 0.9, // Higher values means the model will take more risks.
       max_tokens: 150, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
