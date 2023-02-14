5450597b5df4670dfcbc2bfa4c0d5709be66d51b
fhaarman <44256277+fhaarman@users.noreply.github.com>
2023-02-14 15:51:37 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,12 +27,12 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `This is a conversation between ChatGPTBuddy and a stranger.\nRelevant information that ChatGPTBuddy knows:\n${prompt}`,
-      temperature: 0.9, // Higher values means the model will take more risks.
-      max_tokens: 150, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      prompt: `This is a conversation between ChatGPTBuddy and a stranger. ChatGPTBuddy is the most intelligent A.I. asstistant in the world.\nRelevant information that ChatGPTBuddy knows:\n${prompt}`,
+      temperature: 0.1, // Higher values means the model will take more risks.
+      max_tokens: 2000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0.6, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
